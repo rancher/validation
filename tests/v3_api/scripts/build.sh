ba55791c688cd6b047a9380e977c17786f5df586
Brandon DePesa <brandon@rancher.com>
2019-04-19 09:12:37 -0700

    Always write pem key with contents from env var.

--- tests/v3_api/scripts/build.sh
+++ tests/v3_api/scripts/build.sh
@@ -15,7 +15,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" && pwd )"
 
 count=0
 while [[ 3 -gt $count ]]; do
-    docker build -q -f Dockerfile.v3api -t rancher-validation-tests .
+    docker build -q --build-arg AWS_SSH_PEM_KEY=$AWS_SSH_PEM_KEY -f Dockerfile.v3api -t rancher-validation-tests .
     if [[ $? -eq 0 ]]; then break; fi
     count=$(($count + 1))
     echo "Repeating failed Docker build ${count} of 3..."
